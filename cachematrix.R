c648f06cd1d79f905c76206249c7ea366118d195
mlchan <mlchan2013@gmail.com>
2014-05-24 14:32:03 -0400

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -3,13 +3,33 @@
 
 ## Write a short comment describing this function
 
-makeCacheMatrix <- function(x = matrix()) {
 
+makeCacheMatrix <- function(x = numeric()) {
+  z <- NULL
+  set <- function(y) {
+    x <<- y
+    z <<- NULL
+  }
+  get <- function() x
+  setmat <- function(solve) z <<- solve
+  getmat <- function() z
+  list(set = set, get = get,
+       setmat = setmat,
+       getmat = getmat)
 }
-
+##like make vector but instead once confirming not already, sets z using solve and sets positionally in the list
 
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  z <- x$getsolve()
+  if(!is.null(z)) {
+    message("getting cached data")
+    return(z)
+  }
+  data <- x$get()
+  z <- solve(data, ...)
+  x$setsolve(z)
+  z
 }
+##like cache mean but again, uses solve instead of mean
